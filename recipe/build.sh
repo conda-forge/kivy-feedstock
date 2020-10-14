c1287dd62bf497eacd8ca08514a92d3456180a49
matham <moiein2000@gmail.com>
2020-10-14 19:20:36 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 if [ "$(uname)" == "Darwin" ]; then
   export USE_X11=0;
   export USE_GSTREAMER=1;
-  export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-  export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
 else
   export USE_X11=1;
   export USE_GSTREAMER=1;

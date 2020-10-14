b864a322af27a56a65c90000faba254247193a55
matham <moiein2000@gmail.com>
2020-10-14 19:32:13 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 if [ "$(uname)" == "Darwin" ]; then
   export USE_X11=0;
   export USE_GSTREAMER=1;
+  export CPATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include
 else
   export USE_X11=1;
   export USE_GSTREAMER=1;

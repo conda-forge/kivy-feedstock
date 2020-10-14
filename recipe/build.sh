823abe4fb14f7e9c09715f9b38b6812468b21a9e
matham <moiein2000@gmail.com>
2020-10-14 18:43:54 -0400

    Try fixing missing os/availability.h.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,6 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 else
   export USE_X11=0;
   export USE_GSTREAMER=1;
+  sudo ln -s /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/* /usr/local/include/
 fi;
-USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed --no-build-isolation .
+USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .

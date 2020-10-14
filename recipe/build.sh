cceafbe85830590d79016739b15dce32aa3e494d
matham <moiein2000@gmail.com>
2020-10-14 19:07:29 -0400

    Fix missing availability.h

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
-if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-  export USE_X11=1;
-  export USE_GSTREAMER=1;
-else
+if [ "$(uname)" == "Darwin" ]; then
   export USE_X11=0;
   export USE_GSTREAMER=1;
-  sudo ln -s /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/* /usr/local/include/
+  export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+else
+  export USE_X11=1;
+  export USE_GSTREAMER=1;
 fi;
 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .

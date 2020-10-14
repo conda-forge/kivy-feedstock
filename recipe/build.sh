2f3f4296f6252a3b8e3830aba94fcf825595709a
matham <moiein2000@gmail.com>
2020-10-14 17:56:30 -0400

    Disable pep 517.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ else
   export USE_X11=0;
   export USE_GSTREAMER=1;
 fi;
-USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
+USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed --no-build-isolation .
